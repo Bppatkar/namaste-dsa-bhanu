edaec59be43dae772e1d50aba08b8a8d8bc126bb
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-02-07 11:22:44 +0530

    leetcode 141 leetcode 234

--- 07_Linked List/index.js
+++ 07_Linked List/index.js
@@ -193,3 +193,99 @@ var reverseList = function (head) {
   head = prev;
   return head;
 };
+
+//! Leetcode 141. Linked List Cycle
+//* approch 1 - using Hashmap or in javascript called Set
+var hasCycle = function (head) {
+  let seenNodes = new Set();
+  let curr = head;
+  while (curr != null) {
+    if (seenNodes.has(curr)) {
+      return true;
+    } else {
+      seenNodes.add(curr);
+      curr = curr.next;
+    }
+  }
+  return false;
+};
+//? we are storing every elem, and using while loop so it will run 'n' times so time complexity O(n) and space complexity O(n)
+
+//! Other approch where we dont use extra space means without set and space complexity O(1)
+//! Floyd's Cycle Finding ALgorithm [slow fast pointer approch]
+
+var hasCycle1 = function (head) {
+  if (head === null) return false;
+  let slow = head;
+  let fast = head.next;
+  while (slow != fast) {
+    if (fast == null || fast.next == null) return false;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return true;
+};
+
+//! Leetcode 234. Palindrome Linked List
+/*
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+//* approch 1 - conver linked list to an array and check it is palindrome
+//? Time complexity: O(n)
+//? Space complexity: O(n) (due to the array storage)
+
+var isPalindrome = function (head) {
+  let curr = head;
+  let arr = [];
+  while (curr != null) {
+    arr.push(curr.val);
+    curr = curr.next;
+  }
+  let mid = Math.floor(arr.length / 2);
+  for (let i = 0; i < mid; i++) {
+    if (arr[i] != arr[arr.length - 1 - i]) return false;
+  }
+  return true;
+};
+
+//* approch 2 - without taking extra space
+/*
+ * find middle of linked list
+ * reverse the second half of the linked list
+ * move start & end point and compare each value
+ * 1->2->3->3->2->1 become 1->2->3->1->2->3 [compare each]
+ */
+
+var isPalindrome1 = function (head) {
+  // finding the middle elem [using slow and fast pointer]
+
+  let slow = head;
+  let fast = head;
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // reverse the second half of list
+  let prev = null;
+  let curr = slow;
+  while (curr) {
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  //? curr targeting null and prev targeting last value
+  // checking for palindrome
+  let firstNode = head;
+  let lastNode = prev;
+  while (lastNode != null) {
+    if (firstNode.val != lastNode.val) {
+      return false;
+    }
+    firstNode = firstNode.next;
+    lastNode = lastNode.next;
+  }
+  return true;
+};
