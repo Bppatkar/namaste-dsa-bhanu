bf7fd75d1cf8a28a6cb59f4ec7ad8740e8321c2b
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-02-10 08:53:15 +0530

    Leetcode - 58. Length of Last Word [2 aprroch done now 3rd one with single loop]

--- 08_Strings/index.js
+++ 08_Strings/index.js
@@ -1,13 +1,13 @@
 //! Leetcode - 58. Length of Last Word
 
-var lengthOfLastWor = function (s) {
+var lengthOfLastWord = function (s) {
   s = s.trim();
   let word = s.split(' ');
   return (word = word[word.length - 1]).length;
 };
 
-//* Dont use built in method
-var lengthOfLastWord = function (s) {
+//! Dont use built in method
+var lengthOfLastWord1 = function (s) {
   // trim all the spaces at the end because we start loop from the end
   // count the character till you reach a space
   let n = s.length - 1;
@@ -18,14 +18,20 @@ var lengthOfLastWord = function (s) {
   }
   // n will the point where my last words has start
   let count = 0;
-  while (s[n] != ' ') {
-    count++;
-    --n;
+  while (n >= 0) {
+    if (s[n] != ' ') {
+      count++;
+      --n;
+    } else break;
   }
   return count;
+};
+//! using single loop
+var lengthOfLastWord2 = function (s) {
+    
 };
 
 let s = 'Hello world';
 // let s = '  fly me  to  the moon  ';
 // let s = 'luffy is still joyboy';
-console.log(lengthOfLastWord(s));
+console.log(lengthOfLastWord2(s));
