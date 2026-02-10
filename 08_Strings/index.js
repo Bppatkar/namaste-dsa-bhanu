b7f684859ce97dca7af4eefaacba2075e2cd2819
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-02-10 22:44:06 +0530

    leetcode 771. Jewels and Stones Leetcode 3541. Find Most Frequent Vowel and Consonant

--- 08_Strings/index.js
+++ 08_Strings/index.js
@@ -38,37 +38,12 @@ var lengthOfLastWord2 = function (s) {
 };
 
 // let s = 'Hello world';
-let s = '  fly me  to  the moon  ';
+// let s = '  fly me  to  the moon  ';
 // let s = 'luffy is still joyboy';
 // console.log(lengthOfLastWord2(s));
 
 //! Leetcode - 2942. Find Words Containing Character
-/* 
- *You are given a 0-indexed array of strings words and a character x.
- * Return an array of indices representing the words that contain the character x.
- * Note that the returned array may be in any order.
 
- * Example 1:
- Input: words = ["leet","code"], x = "e"
-  Output: [0,1]
-  Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
-
- * Example 2:
-  Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
-  Output: [0,2]
-  Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
-
- * Example 3:
-  Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
-  Output: []
-  Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
-
- * Constraints:
-  1 <= words.length <= 50
-  1 <= words[i].length <= 50
-  x is a lowercase English letter.
-  words[i] consists only of lowercase English letters.
- */
 //! Using built-in method (includes)
 var findWordsContaining = function (words, x) {
   let arr = [];
@@ -99,10 +74,114 @@ var findWordsContaining2 = function (words, x) {
   }
   return res;
 };
-let words = ['abc', 'bcd', 'aaaa', 'cbc'],
-  x = 'a';
+// let words = ['abc', 'bcd', 'aaaa', 'cbc'],
+//   x = 'a';
 // let words = ['leet', 'code'],
 //   x = 'e';
 // let words = ['abc', 'bcd', 'aaaa', 'cbc'],
 //   x = 'z';
-console.log(findWordsContaining(words, x));
+// console.log(findWordsContaining(words, x));
+
+//! Leetcode 771. Jewels and Stones
+//! Using Built In Method
+// var numJewelsInStones = function (jewels, stones) {
+//   let count = 0;
+//   for (let i = 0; i < stones.length; i++) {
+//     if (jewels.includes(stones[i])) count++;
+//   }
+//   return count;
+// };
+//! Without Built In Method
+var numJewelsInStones = function (jewels, stones) {
+  let count = 0;
+  for (let i = 0; i < stones.length; i++) {
+    for (let j = 0; j < jewels.length; j++) {
+      if (stones[i] === jewels[j]) count++;
+    }
+  }
+  return count;
+};
+//! More Optimise Approch using - HashMap or Set()
+var numJewelsInStones = function (jewels, stones) {
+  let jSet = new Set();
+  let count = 0;
+  for (let i = 0; i < jewels.length; i++) {
+    jSet.add(jewels[i]);
+  }
+  for (let i = 0; i < stones.length; i++) {
+    if (jSet.has(stones[i])) count++;
+  }
+  return count;
+};
+// let jewels = 'aA',
+//   stones = 'aAAbbbb';
+// let jewels = 'z',
+//   stones = 'ZZ';
+// console.log(numJewelsInStones(jewels, stones));
+
+//! Leetcode 3541. Find Most Frequent Vowel and Consonant
+var maxFreqSum = function (s) {
+ let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+
+  let vCount = {}, cCount = {}, vMax = 0, cMax = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let ch = s[i];
+    let flag = false;
+
+    for (let j = 0; j < vowels.length; j++) {
+      if (vowels[j] === ch) {
+        vCount[ch] = (vCount[ch] || 0) + 1;
+        // updating the max while counting
+        if (vCount[ch] > vMax) vMax = vCount[ch];
+        flag = true;
+        break;
+      }
+    }
+
+    //* if not found in vowels so check in consonants
+    if (!flag) {
+      for (let j = 0; j < consonants.length; j++) {
+        if (consonants[j] === ch) {
+          cCount[ch] = (cCount[ch] || 0) + 1;
+          // updating the max while counting
+          if (cCount[ch] > cMax) cMax = cCount[ch];
+          break;
+        }
+      }
+    }
+  }
+
+  console.log('v-count final', vMax);
+  console.log('c-count final', cMax);
+  return vMax + cMax;
+};
+
+//! Built In method - [includes & Math.max()]
+var maxFreqSum = function (s) {
+  let map = {};
+  for (let i = 0; i < s.length; i++) {
+    map[s[i]] = !map[s[i]] ? 1 : ++map[s[i]];
+  }
+
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let maxVowel = 0;
+  let maxConsonant = 0;
+  // running loop on map keys not the whole string [Optimisation]
+  let mapKey = Object.keys(map);
+
+   for (let i = 0; i < mapKey.length; i++) {
+    if (vowels.includes(mapKey[i])) {
+      // if (map[mapKey[i]] > maxVowel) maxVowel = map[mapKey[i]];
+      maxVowel = Math.max(map[mapKey[i]],maxVowel)
+    } else {
+      // if (map[mapKey[i]] > maxConsonant) maxConsonant = map[mapKey[i]];
+      maxConsonant = Math.max(map[mapKey[i]],maxConsonant)
+    }
+  }
+  return maxVowel + maxConsonant;
+};
+let s = 'successes'; // 2+4=6
+// let s = 'aeiaeia'; // 3+0=3
+console.log(maxFreqSum(s));
