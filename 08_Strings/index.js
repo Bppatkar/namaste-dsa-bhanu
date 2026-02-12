d4154af1023284de2c67a92df4764d50d0187bc7
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-02-12 17:09:16 +0530

    Leetcode 14. Longest Common Prefix Leetcode 14. Longest Common Prefix Leetcode 1903. Largest Odd Number in String

--- 08_Strings/index.js
+++ 08_Strings/index.js
@@ -295,20 +295,70 @@ var isPalindrome = function (s) {
 
 //! More Optimisation - S-O(1) [using Two Pointer approach]
 var isPalindrome = function (s) {
+  s = s.toLowerCase();
   let i = 0;
   let j = s.length - 1;
   while (i < j) {
     if (!s[i].match(/[a-z0-9]/i)) ++i;
     else if (!s[j].match(/[a-z0-9]/i)) --j;
-    else if (s[i].toLowerCase() === s[j].toLowerCase()) {++i; --j;}
-    else {return false};
+    else if (s[i] === s[j]) {
+      ++i;
+      --j;
+    } else {
+      return false;
+    }
   }
-  return true
+  return true;
 };
 
-let s = 'A man, a plan, a canal: Panama';
+// let s = 'A man, a plan, a canal: Panama';
 // let s = "race a car"
 // let s = ''
-console.log(isPalindrome(s));
+// console.log(isPalindrome(s));
 
-//!
+//! Leetcode 1903. Largest Odd Number in String
+var largestOddNumber = function (num) {
+  num = num.split('');
+  for (let i = num.length - 1; i >= 0; i--) {
+    if (num[i] % 2 === 0) num.pop();
+    else if (num[i] % 2 != 0) return num.join('');
+  }
+  return num.join('');
+};
+
+var largestOddNumber = function (num) {
+  let n = num.length - 1;
+  while (n >= 0) {
+    if (num[n] % 2 === 1) {
+      return num.substring(0, n + 1)
+    }
+    --n;
+  }
+  return "";
+}
+// let num = '52';
+// let num = '4206';
+// let num = '  35427';
+// console.log(largestOddNumber(num));
+
+//! Leetcode 14. Longest Common Prefix
+var longestCommonPrefix = function (strs) {
+  let x = 0;
+  while (x < strs[0].length) {
+    let char = strs[0][x];
+    for (let i = 1; i < strs.length; i++) {
+      if (char != strs[i][x] || x === strs[i].length) return strs[0].substring(0, x)
+    }
+    ++x;
+  }
+  return strs[0]
+}
+let strs = ["flower", "flow", "flight"] // "fl"
+// let strs = ["dog", "racecar", "car"] // ""
+// console.log(longestCommonPrefix(strs))
+
+
+//! Leetcode 242. Valid Anagram
+ var isAnagram = function(s, t) {
+    
+};
