33dc14cc9dcc9a3b7c4f81b07bfb7e67801b9521
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-02-15 09:03:00 +0530

    Leetcode 225 ✔ [with one queue ✔]

--- 09_Stack and Queues/leetcode.js
+++ 09_Stack and Queues/leetcode.js
@@ -42,8 +42,14 @@ All the calls to pop and top are valid.
 Follow-up: Can you implement the stack using only one queue?
 */
 
-var MyStack = function () {
 
+// ----------------------------------------------------
+//! Implementing Stack using Two Queue
+// ----------------------------------------------------
+
+var MyStack = function () {
+  this.q1 = [];
+  this.q2 = [];
 };
 
 /** 
@@ -51,28 +57,109 @@ var MyStack = function () {
  * @return {void}
  */
 MyStack.prototype.push = function (x) {
-
+  this.q1.push(x)
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.pop = function () {
+  let n = this.q1.length;
 
+  for (let i = 0; i < n - 1; i++) {
+    this.q2.push(this.q1.shift())
+  }
+  let ans = this.q1.shift()
+
+  // exchange q1 and q2
+  let temp = this.q1;
+  this.q1 = this.q2;
+  this.q2 = temp;
+
+  return ans;
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.top = function () {
+  let n = this.q1.length;
+  for (let i = 0; i < n - 1; i++) {
+    this.q2.push(this.q1.shift());
+  }
+  let front = this.q1[0];
 
+  this.q2.push(this.q1.shift());
+
+  // exchange q1 and q2
+  let temp = this.q1;
+  this.q1 = this.q2;
+  this.q2 = temp;
+
+  return front;
 };
 
 /**
  * @return {boolean}
  */
 MyStack.prototype.empty = function () {
+  return this.q1.length === 0;
+};
 
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+
+// ----------------------------------------------------
+//! Implementing Stack using One Queue
+// ----------------------------------------------------
+
+var MyStack = function () {
+  this.q1 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  this.q1.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  let n = this.q1.length;
+  for (let i = 0; i < n - 1; i++) {
+    this.q1.push(this.q1.shift())
+  }
+  return this.q1.shift()
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  let n = this.q1.length;
+  for (let i = 0; i < n - 1; i++) {
+    this.q1.push(this.q1.shift())
+  }
+  let front = this.q1[0];
+  this.q1.push(this.q1.shift())
+  return front;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  return this.q1.length === 0;
 };
 
 /** 
