159233b95d7dd741e859c0a7af2284bb0fb4446f
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-02-19 18:15:51 +0530

    Leetcode 33. Search in Rotated Sorted Array Leetcode 278. First Bad Version Leetcode 162. Find Peak Element [using both linear and binary search]

--- 10_Binary Search Algorithm/index.js
+++ 10_Binary Search Algorithm/index.js
@@ -61,33 +61,100 @@ var guessNumber = function (n) {
 // let n = 1, pick = 1 // Output: 1
 // let n = 2, pick = 1 // Output: 1
 
-
-
 // ----------------------------------------------------
 //! Leetcode 33. Search in Rotated Sorted Array
 // ----------------------------------------------------
 var search = function (arr, target) {
-  let left = 0, right = arr.length;
+  let left = 0, right = nums.length - 1;
 
   while (left <= right) {
-    let mid = left + Math.floor((right - left) / 2);
-    if (target === arr[mid]) return mid;
 
-    // left sorted array
-    if (arr[left] < arr[mid]) {
-      if (target < arr[mid] && target >= arr[left]) right = mid - 1;
-      else left = mid + 1;
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (target === nums[mid]) return mid;
+
+    // if left side is sorted
+    if (nums[left] <= nums[mid]) {
+      if (target < nums[mid] && target >= nums[left]) right = mid - 1;
+      else left = mid + 1
     }
-    // right sorted array
+
+    // if right side is sorted
     else {
-      if (target > arr[mid] && target < arr[right]) left = mid + 1;
+      if (target > nums[mid] && target <= nums[right]) left = mid + 1;
       else right = mid - 1;
     }
-  }
 
-  return -1
+  }
+  return -1;
 };
-let nums = [4, 5, 6, 7, 0, 1, 2], target = 0 // Output: 4
+// let nums = [4, 5, 6, 7, 0, 1, 2], target = 0 // Output: 4
 // let nums = [4,5,6,7,0,1,2], target = 3 // Output: -1
 // let nums = [1], target = 0 // Output: -1
-console.log(search(nums, target))
+// let nums = [5,1,3], target = 3 // Output 2
+// console.log(search(nums, target))
+
+// ----------------------------------------------------
+//! Leetcode 278. First Bad Version
+// ----------------------------------------------------
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let l = 0, r = n;
+    while (l < r) {
+      let m = l + Math.floor((r - l) / 2);
+      if (!isBadVersion(m)) l = m + 1;
+      else r = m;
+    }
+    return r;
+  };
+};
+
+// ----------------------------------------------------
+//! Leetcode 162. Find Peak Element
+// ----------------------------------------------------
+//! Using Linear search T- O(n) and S - O(n)
+var findPeakElement = function (nums) {
+  if (nums.length === 1) return 0;
+
+  if (nums[0] > nums[1]) return 0;
+
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i
+  } 
+  
+  if (nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
+
+  return -1;
+};
+
+//! using Binary search O(logn)
+var findPeakElement = function (nums) {
+  let l = 0, r = nums.length - 1;
+  while (l < r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (nums[m] < nums[m + 1]) l = m + 1
+    else r = m;
+  }
+  return l // we can return l or r anything because when l == r then our loop should end and that is our ans
+}
+// Test Case 1: Standard case with peak in middle
+console.log("Test Case 1 [1, 2, 3, 1]:", findPeakElement([1, 2, 3, 1])); // Expected: 2
+
+// Test Case 2: Multiple peaks (return any valid peak)
+console.log("Test Case 2 [1, 2, 1, 3, 5, 6, 4]:", findPeakElement([1, 2, 1, 3, 5, 6, 4])); // Expected: 1 or 5
+
+// Test Case 3: Ascending array (peak at end)
+console.log("Test Case 3 [1, 2, 3, 4, 5]:", findPeakElement([1, 2, 3, 4, 5])); // Expected: 4
+
+// Test Case 4: Descending array (peak at start)
+console.log("Test Case 4 [5, 4, 3, 2, 1]:", findPeakElement([5, 4, 3, 2, 1])); // Expected: 0
+
+// Test Case 5: Single element
+console.log("Test Case 5 [1]:", findPeakElement([1])); // Expected: 0
+
+// Test Case 6: Two elements
+console.log("Test Case 6 [1, 2]:", findPeakElement([1, 2])); // Expected: 1
