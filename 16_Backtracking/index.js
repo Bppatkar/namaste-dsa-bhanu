d89cdec65288684c72acfc37ef83d8de94565c0a
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-03-26 17:30:18 +0530

    Leetcode 78. Subsets

--- 16_Backtracking/index.js
+++ 16_Backtracking/index.js
@@ -1,4 +1,19 @@
 //! Leetcode 78. Subsets
-var subsets= function(nums){
-  
+var subsets = function (nums) {
+  let result = [];
+
+  const backtrack = (path, start) => {
+    result.push([...path]);
+
+    for (let i = start; i < nums.length; i++) {
+      path.push(nums[i]);
+      backtrack(path, i + 1);
+      path.pop();
+    }
+  }
+  backtrack([], 0);
+  return result;
 }
+
+console.log(subsets(nums = [1, 2, 3])) // Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+console.log(subsets(nums = [0])) // Output: [[],[0]]
