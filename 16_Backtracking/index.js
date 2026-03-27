5ab3f665651630c5b84892622dd95b43e6669cae
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-03-27 14:45:12 +0530

    Leetcode 77. Combinations

--- 16_Backtracking/index.js
+++ 16_Backtracking/index.js
@@ -11,9 +11,29 @@ var subsets = function (nums) {
       path.pop();
     }
   }
-  backtrack([], 0);
+  backtrack([], 0); // starting from empty nodes and 0 indexing
   return result;
 }
 
-console.log(subsets(nums = [1, 2, 3])) // Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-console.log(subsets(nums = [0])) // Output: [[],[0]]
+// console.log(subsets(nums = [1, 2, 3])) // Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// console.log(subsets(nums = [0])) // Output: [[],[0]]
+
+//! Leetcode 77. Combinations
+var combine = function (n, k) {
+  let result = [];
+
+  const backtrack = (path, start) => {
+    if (path.length === k) return result.push([...path]);
+
+    for (let i = start; i <= n; i++) {
+      path.push(i);
+      backtrack(path, i + 1);
+      path.pop();
+    }
+  }
+  backtrack([], 1);
+  return result;
+};
+
+console.log(combine(n = 4, k = 2)) //output - [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+console.log(combine(n = 1, k = 1)) //output - [[1]]
