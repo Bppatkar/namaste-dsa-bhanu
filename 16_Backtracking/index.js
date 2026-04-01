63aca5eb66641409b2e68eb9eb3b392d35ca00c0
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-04-01 15:10:05 +0530

    Leetcode 79. Word Search

--- 16_Backtracking/index.js
+++ 16_Backtracking/index.js
@@ -237,5 +237,49 @@ const isPalindrome = (s) => {
   return true;
 }
 
-console.log(partition(s = "aab")) // Output: [["a","a","b"],["aa","b"]]
-console.log(partition(s = "a")) // Output: [["a"]]
+// console.log(partition(s = "aab")) // Output: [["a","a","b"],["aa","b"]]
+// console.log(partition(s = "a")) // Output: [["a"]]
+
+//! Leetcode 79. Word Search
+var exist = function (board, word) {
+  let result = false;
+
+  let m = board.length;
+  let n = board[0].length;
+
+  let backtrack = (x, y, nextIndex) => {
+    if (nextIndex === word.length) result = true;
+
+    let original = board[x][y];
+    board[x][y] = '#'
+
+    if (y < n - 1 && board[x][y + 1] === word[nextIndex]) {
+      backtrack(x, y + 1, nextIndex + 1);
+    }
+    if (y > 0 && board[x][y - 1] === word[nextIndex]) {
+      backtrack(x, y - 1, nextIndex + 1);
+    }
+    if (x > 0 && board[x - 1][y] === word[nextIndex]) {
+      backtrack(x - 1, y, nextIndex + 1);
+    }
+    if (x < m - 1 && board[x + 1][y] === word[nextIndex]) {
+      backtrack(x + 1, y, nextIndex + 1);
+    }
+
+    board[x][y] = original;
+  }
+
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === word[0]) {
+        backtrack(i, j, 1);
+      }
+    }
+  }
+  return result;
+}
+
+// console.log(exist(board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], word = "ABCCED")) // Output: true
+// console.log(exist(board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], word = "SEE")) // Output: true
+// console.log(exist(board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], word = "ABCB")) // Output: false
