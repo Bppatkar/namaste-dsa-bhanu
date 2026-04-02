a890cb90ae5f523cd2a3c1ea27c619a6dfff35a4
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-04-02 11:08:42 +0530

    leetcode 51 N-Queen done backtracking complete

--- 16_Backtracking/index.js
+++ 16_Backtracking/index.js
@@ -283,3 +283,43 @@ var exist = function (board, word) {
 // console.log(exist(board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], word = "ABCCED")) // Output: true
 // console.log(exist(board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], word = "SEE")) // Output: true
 // console.log(exist(board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], word = "ABCB")) // Output: false
+
+//! Leetcode 51. N-Queens
+var solveNQueens = function (n) {
+  let result = [];
+  let board = Array.from({ length: n }, () => Array(n).fill("."));
+  let backtrack = (board, row, colSet, digSet, antiDigSet) => {
+    if (row == n) {
+      result.push(transform(board));
+    }
+    for (let col = 0; col < n; col++) {
+      if (colSet.has(col) ||
+        digSet.has(row - col) ||
+        antiDigSet.has(row + col)) {
+        continue;
+      }
+      board[row][col] = "Q";
+      colSet.add(col);
+      digSet.add(row - col);
+      antiDigSet.add(row + col);
+
+      backtrack(board, row + 1, colSet, digSet, antiDigSet);
+      board[row][col] = ".";
+      colSet.delete(col);
+      digSet.delete(row - col);
+      antiDigSet.delete(row + col);
+    }
+  };
+  backtrack(board, 0, new Set(), new Set(), new Set());
+  return result;
+};
+
+let transform = (board) => {
+  let newBoard = [];
+  for (let i = 0; i < board.length; i++) {
+    newBoard.push(board[i].join(""));
+  }
+  return newBoard;
+}
+
+console.log(solveNQueens(n = 4));
