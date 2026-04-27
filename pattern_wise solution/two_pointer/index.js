e338a5554202d4640fd0b26d81f5e4220f867664
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-04-27 21:09:38 +0530

    updating two pointer question- solutions

--- pattern_wise solution/two_pointer/index.js
+++ pattern_wise solution/two_pointer/index.js
@@ -16,6 +16,8 @@
  * 5. Multiple Pointers: More than two pointers to solve complex problems (e.g., 3-sum).  
  *  for instance - in 3 sum problem, we can fix one pointer and use two pointers to find pairs that sum up to the negative of the fixed pointer's value.  
  * 
+ * 
+ *! 'Comparision', 'searching' [pair of elements], and 'partitioning' [PCS - (partitioning, comparison, searching)] problems are commonly solved using two pointer technique.
  */
 
 
@@ -42,8 +44,79 @@ var reverseString = function (s) {
     i++;
     j--;
   }
-  return s;
 }
 
 //! Leetcode 977. Squares of a Sorted Array
-var sortedSquares = function (nums) { }
+var sortedSquares = function (nums) {
+  let ans = [], i = 0, j = nums.length - 1;
+  while (i <= j) {
+    if (Math.abs(nums[i] ** 2) < Math.abs(nums[j] ** 2)) {
+      ans.unshift(nums[j] ** 2);
+      j--;
+    } else {
+      ans.unshift(nums[i] ** 2);
+      i++;
+    }
+  }
+
+  return ans;
+}
+
+
+//! Leetcode 680. Valid Palindrome II
+var validPalindrome = function (s) {
+  let i = 0, j = s.length - 1;
+  while (i < j) {
+    if (s[i] != s[j]) {
+      // we can either skip the left character (i) or the right character (j) and check if the remaining substring is a palindrome
+      return isPalindromeRange(s, i + 1, j) || isPalindromeRange(s, i, j - 1);
+    }
+    i++;
+    j--;
+  }
+  return true;
+}
+
+const isPalindromeRange = (s, i, j) => {
+  while (i < j) {
+    if (s[i] != s[j]) return false;
+    i++;
+    j--;
+  }
+  return true;
+}
+
+
+//* Neetcode 408. Valid Word Abbreviation
+var validWordAbbreviation = function (word, abbr) {
+  // step 1 - initialize two pointers for word and abbr
+  // step 2 - iterate through both strings until we reach the end of either string
+  // step 3 - if characters match, move both pointers
+  // step 4 - if characters don't match, check if the current character in abbr is a digit and if it is, calculate the number and move the word pointer accordingly and if it's not a digit, return false
+  let i = 0, j = 0;
+  while (i < word.length && j < abbr.length) {
+    if (word[i] === abbr[j]) {
+      i++;
+      j++;
+    }
+    else if (abbr[j] >= '0' && abbr[j] <= '9') {
+      if (abbr[j] === '0') return false;
+    }
+    else {
+      return false;
+    }
+    // calculate the number and move the word pointer accordingly
+
+    let num = 0;
+    while (j < abbr.length && abbr[j] >= '0' && abbr[j] <= '9') {
+      num = num * 10 + (abbr[j] - '0');
+      j++;
+    }
+    i += num;
+
+  }
+  return i === word.length && j === abbr.length;
+}
+
+console.log(validWordAbbreviation("internationalization", "i12iz4n")) // true
+console.log(validWordAbbreviation("apple", "a2e")) // false
